4ab6ba68e565ad78e2736990f9877c8f2364140a
krusherub <52534389+krusherub@users.noreply.github.com>
2022-02-15 10:06:31 +0200

    Add files via upload

---
+++ task1.lang_with_go_to/Program.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+public class Program
+{
+    public static void Main()
+    {
+        string path = "D:\\Steam\\text.txt"; //absolute path
+        //string path = "text.txt"; //path
+        StreamReader s = new StreamReader(path);
+        string text = s.ReadToEnd() + " ";
+        Console.WriteLine(text);
+
+        string[] prepositions = new string[] {"in","for", "at","on","to"};
+        string[] wordsWithoutRep;
+        int[] countWithoutRep;
+        
+        int count = 0;
+        int j = 0;// for MainCycle
+        int k = 0;// for MainCycle
+        
+        int i = 0; //index
+        string[] words; // all words
+        Count: //Count of words
+        if (i == text.Length - 1)
+        {
+            count++;
+            i = 0;
+            words = new string[count];
+            goto MainCycle;
+        }
+        if (text[i] == ' ' || text[i] == '\n')
+        { 
+            count++;
+        }
+        i++;
+        goto Count;
+        
+        MainCycle:
+            if (text[i] == ' ' || text[i] == '\n') // added i
+            {
+                string word = "";
+                Word:
+                    if (k != i)
+                    {
+                        if (text[k] != '\r')
+                        {
+                            if (text[k] > 64 && text[k] < 91)
+                            {
+                                char c = (char)(text[k] + 32);
+                                word += c;
+                            }
+                            else
+                            {
+                                word += text[k];
+                            }
+                        }
+                    }
+                    else // всё работало без проверки
+                    {
+                        int p = 0;
+                        CheckPreposition:
+                            if (p >= prepositions.Length)
+                                goto ExitCheck;
+                            if (word == prepositions[p])
+                            {
+                                //j++;
+                                k++;
+                                goto ExitIf;
+                            }
+                            p++;
+                        goto CheckPreposition;
+                        ExitCheck:
+                        words[j] = word;
+                        j++;
+                        k++;
+                        goto ExitIf;
+                    }
+                    k++;
+                goto Word;
+            }
+            ExitIf:
+            if (i == text.Length - 1)
+            {
+                i = 0;
+                j = 0;
+                k = 0;
+                wordsWithoutRep = new string[count];
+                countWithoutRep = new int[count];
+                goto GetCount1;
+            }
+            i++;
+                
+        goto MainCycle;
+        
+        GetCount1:
+            if (i > count - 1)
+            {
+                i = 1;
+                j = 0;
+                goto Buble1;
+            }
+            j = 0;
+            GetCount2:
+                if(j > count - 1) // leave circle
+                    goto ExitCount2;
+                if (words[i] == wordsWithoutRep[j])
+                {
+                    countWithoutRep[j]++;
+                    j++;
+                    goto ExitCount2;
+                }
+                else if(wordsWithoutRep[j] == null)
+                {
+                    wordsWithoutRep[k] = words[i];
+                    countWithoutRep[k]++;
+                    k++;
+                    goto ExitCount2;
+                }
+                j++;
+            goto GetCount2;
+            
+            ExitCount2:
+            i++;
+        goto GetCount1;
+        
+        Buble1:
+            if (i >= k)
+            {
+                i = 0;
+                goto Finish;  
+            }
+
+            j = 0;
+            Buble2: //cycle
+                if(j >= k - i)
+                    goto ExitBuble;
+                if (countWithoutRep[j] < countWithoutRep[j + 1])
+                {
+                    //swap counts
+                    int temp = countWithoutRep[j];
+                    countWithoutRep[j] = countWithoutRep[j + 1];
+                    countWithoutRep[j + 1] = temp;
+                    //swap words
+                    string temp1 = wordsWithoutRep[j];
+                    wordsWithoutRep[j] = wordsWithoutRep[j + 1];
+                    wordsWithoutRep[j + 1] = temp1;
+                }
+                j++;
+            goto Buble2;
+            
+            ExitBuble:
+            i++;
+        goto Buble1;
+
+        Finish:
+            if(wordsWithoutRep[i] == null)
+                goto Stop;
+            Console.WriteLine(wordsWithoutRep[i] + " - " + countWithoutRep[i]);
+            i++;
+        goto Finish;
+        
+        Stop: ;
+    }
+
+}
---
+++ task2.lang_with_go_to/Program.cs
@@ -0,0 +1,373 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+public class Program
+{
+    public static void Main()
+    {
+        string path = "D:\\Steam\\text2.txt"; //absolute path
+        StreamReader s = new StreamReader(path);
+        string text = s.ReadToEnd();
+        //Console.WriteLine(text);
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        int l = 0; // letter
+        int p = 0;
+        int count = 1;
+        int limitPage = 45; // count of lines in a page
+        string newText = "";
+        string signs = ".,\"“”!?;";
+        string[] resultWords;
+       // int wordsNumberInPage = 0;
+        string[] lines;
+        string[] pages;
+        string[,] words;
+        DeleteSign:
+            if (i >= text.Length)
+            {
+                i = 0;
+                j = 0;
+                lines = new string[count];
+                InitLines:
+                    if (i >= count)
+                    {
+                        i = 0;
+                        goto ExitInitPages;
+                    }
+                    lines[i] = "";
+                    i++;
+                goto InitLines;
+                ExitInitPages:
+                goto Lines;
+            }
+
+            if (text[i] == '\n')
+            {
+                count++;
+            }
+            
+            j = 0;
+            DeleteSign2:
+                if (j >= signs.Length)
+                {
+                    newText += text[i];
+                    goto ExitDeleteSign2;
+                }
+                if (text[i] == signs[j])
+                {
+                    //newText += "";
+                    goto ExitDeleteSign2;
+                }
+                j++;
+            goto DeleteSign2;
+            ExitDeleteSign2:
+            i++;
+        goto DeleteSign;
+        
+        Lines:
+            if (i  >= newText.Length)
+            {
+                i = 0;
+                j = 0;
+                pages = new string[lines.Length/limitPage + 1 ];
+                goto Pages;
+            }
+            if (newText[i] != '\r' && newText[i] != '\n')
+            {
+                lines[j] += newText[i];
+                i++;
+            }
+            else
+            {
+                j++;
+                i += 2;
+            }
+        goto Lines;
+        
+        Pages:
+            if (i >= pages.Length || j >= lines.Length)
+            {
+                words = new string[pages.Length,5000]; // 5k words - 1 page
+                i = 0;
+                j = 0;
+                l = 0;
+                goto Words;
+            }
+
+            if (j % limitPage != 0 || j == 0)
+            {
+                if(lines[j] != "")
+                    pages[i] += lines[j] + " "; 
+                j++;
+            }
+            else
+            {
+                i++;
+                if(lines[j] != "")
+                    pages[i] += lines[j] + " ";
+                j++;
+            }
+            
+            //j++;
+        goto Pages;
+
+        Words:
+            if (i >= pages.Length)
+            {
+                i = 0;
+                j = 0;
+                k = 0;
+                l = 0;
+                goto DeleteRep; 
+            }
+            Words1:
+            if (pages[i][l] == ' ') 
+            {
+                string word = "";
+                Word:
+                if (k != l)
+                {
+                    if (pages[i][k] != '\r')
+                    {
+                        if (pages[i][k] > 64 && pages[i][k] < 91)
+                        {
+                            char c = (char)(pages[i][k] + 32);
+                            word += c;
+                        }
+                        else
+                        {
+                            word += pages[i][k];
+                        }
+                    }
+                }
+                else // всё работало без проверки
+                {
+                    words[i,j] = word;
+                    j++;
+                    k++;
+                    goto ExitIf;
+                }
+                k++;
+                goto Word;
+            }
+            ExitIf:
+            if (l == pages[i].Length - 1)
+            {
+                j = 0;
+                k = 0;
+                l = 0;
+                goto ExitWords1;
+            }
+            l++;
+            goto Words1;
+            ExitWords1:
+            i++;
+            goto Words;
+        
+        DeleteRep: // page cycle
+            if (i >= pages.Length)
+            {
+                i = 0;
+                j = 0;
+                k = 0;
+                l = 0;
+                resultWords = new string[pages.Length * 5000];
+                goto ResultPages;
+            }
+            DeleteRep1://word cycle
+                if (words[i,j] == null)
+                {
+                    j = 0;
+                    k = 0;
+                    goto ExitDeleteRep1;
+                }
+                string currWord = words[i, j];
+                DeleteRep2: //check word cycle
+                    if (words[i,k] == null)
+                    {
+                        k = 0; // need to rest
+                        goto ExitDeleteRep2;
+                    }
+
+                    if (currWord == words[i, k] && j != k)
+                    {
+                        words[i, k] = "";
+                    }
+    
+                    k++; //check word
+                goto DeleteRep2;
+                
+                ExitDeleteRep2: // goto next word
+                
+                j++; //word
+            goto DeleteRep1;
+            
+            ExitDeleteRep1: // GOTO next page
+            i++; //page
+        goto DeleteRep;
+        
+        ResultPages: // page
+           // Console.WriteLine(i);
+            if (i >= pages.Length)
+            {
+                i = 0;
+                j = 0;
+                k = 0;
+                goto Check100;
+            }
+            ResultPages1: // word
+               // Console.WriteLine(j);
+                if (words[i,j] == null)
+                {
+                    j = 0;
+                    l = 0;
+                    k = 0;
+                    goto ExitResultPages1;
+                }
+                if (words[i, j] == "")
+                {
+                    j++;
+                    goto ResultPages1;
+                }
+                string word1 = words[i, j]; // page-word
+                resultWords[p] = word1 + " - " + (i+1) + ", ";
+                ResultPages2:
+                   // Console.WriteLine(l);
+                    if (l >= pages.Length)
+                    {
+                        l = 0;
+                        k = 0;
+                        goto ExitResultPages2;
+                    }
+
+                    if (l == i)
+                    {
+                        l++;
+                        goto ResultPages2;
+                    }
+                    
+                    ResultPages3:
+                       // Console.WriteLine(k);
+                        if (words[l, k] == null)
+                        {
+                            k = 0;
+                            goto ExitResultPages3;
+                        }
+
+                        if (words[l,k] == "")
+                        {
+                            k++;
+                            goto ResultPages3;
+                        }
+
+                        if (word1 == words[l, k] && l != i)
+                        {
+                            words[l, k] = "";
+                            resultWords[p] += (l+1) + ", ";
+                        }
+
+                        k++;
+                    goto ResultPages3;
+                    
+                    ExitResultPages3:
+
+
+                    l++;
+                goto ResultPages2;
+
+                ExitResultPages2:
+                j++;
+                p++; // ??????
+            goto ResultPages1;
+            
+            ExitResultPages1: // goto next page
+            i++;
+        goto ResultPages;
+        
+        Check100:
+            if (resultWords[i] == null)
+            {
+                i = 0;
+                j = 0;
+                goto Sort;
+            }
+            Check1001:
+                if (j >= resultWords[i].Length)
+                {
+                    j = 0;
+                    goto Exit;
+                }
+                if (resultWords[i][j] == ',')
+                {
+                    k++;
+                }
+                j++;
+            goto Check1001;
+            
+            Exit:
+            if (k >= 100)
+            {
+                resultWords[i] = "";
+               // k = 0;
+            }
+
+            k = 0;
+            i++;
+        goto Check100; 
+        
+        Sort:
+            if (resultWords[i] == null)
+            {
+                i = 0;
+                j = 0;
+                goto Finish;
+            }
+            j = i + 1;
+            Sort1:
+                
+                if (resultWords[j] == null)
+                {
+                    j = 0;
+                    goto ExitSort1;
+                }
+
+                if (resultWords[i][0] > resultWords[j][0])
+                {
+                    string temp = resultWords[i];
+                    resultWords[i] = resultWords[j];
+                    resultWords[j] = temp;
+                    j++;
+                    goto Sort;
+                }
+
+                if (resultWords[i][0] == resultWords[j][0] && resultWords[i][1] > resultWords[j][1])
+                {
+                    string temp = resultWords[i];
+                    resultWords[i] = resultWords[j];
+                    resultWords[j] = temp;
+                    j++;
+                    goto Sort;
+                }
+                j++;
+                goto Sort1;
+            
+            ExitSort1:
+            i++;
+        goto Sort;
+        
+        Finish:
+        if (resultWords[i] == null)
+        {
+            i = 0;
+            j = 0;
+            goto Next;
+        }
+        if(resultWords[i] != "")
+            Console.WriteLine(resultWords[i]);
+        i++;
+        goto Finish;
+        Next: ;
+    }
+}
